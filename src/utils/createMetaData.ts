a7a41313bc2bc5cb504791d0dc0d58c8080e5af1
Tarik <tariksnow37@gmail.com>
2021-01-10 22:31:31 -0500

    Create meta data function

---
+++ src/utils/createMetaData.ts
@@ -0,0 +1,26 @@
+import { Metadata } from '@polkadot/metadata';
+import { TypeRegistry } from '@polkadot/types';
+import memoizee from 'memoizee';
+
+/**
+ * From a metadata hex string (for example returned by RPC), create a Metadata
+ * class instance. Metadata decoding is expensive, so this function is
+ * memoized.
+ *
+ * @ignore
+ * @param registry - The registry of the metadata.
+ * @param metadata - The metadata as hex string.
+ */
+function createMetadataUnmemoized(
+    registry: TypeRegistry,
+    metadataRpc: string
+): Metadata {
+    return new Metadata(registry, metadataRpc);
+}
+
+/**
+ * @ignore
+ */
+export const createMetadata = memoizee(createMetadataUnmemoized, {
+    length: 2,
+});
